e54eba1e21a12537b09d864b62316f53dceecb12
Abhishek <prabheen09876@gmail.com>
2024-09-19 22:46:32 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+gsap.to('.card1', {
+    scale: 0.6,
+    opacity: 0,
+    scrollTrigger: {
+        trigger: '.card1',
+        start: 'top 15%',
+        end: 'bottom 15%',
+        scrub: true,
+    },
+})
+gsap.to('.card2', {
+    scale: 0.6,
+    opacity: 0,
+    scrollTrigger: {
+        trigger: '.card2',
+        start: 'top 15%',
+        end: 'bottom 15%',
+        scrub: true,
+    },
+})
+gsap.to('.card3', {
+    scale: 0.6,
+    opacity: 0,
+    scrollTrigger: {
+        trigger: '.card3',
+        start: 'top 15%',
+        end: 'bottom 15%',
+        scrub: true,
+    },
+})
