b657fb4125bc041b73489fff9b8c081f2c118154
Abhii <prabheen09876@gmail.com>
2024-09-23 07:58:49 +0000

    added more cards

--- script.js
+++ script.js
@@ -1,30 +1,17 @@
-gsap.to('.card1', {
-    scale: 0.6,
-    opacity: 0,
-    scrollTrigger: {
-        trigger: '.card1',
-        start: 'top 15%',
-        end: 'bottom 15%',
-        scrub: true,
-    },
-})
-gsap.to('.card2', {
-    scale: 0.6,
-    opacity: 0,
-    scrollTrigger: {
-        trigger: '.card2',
-        start: 'top 15%',
-        end: 'bottom 15%',
-        scrub: true,
-    },
-})
-gsap.to('.card3', {
-    scale: 0.6,
-    opacity: 0,
-    scrollTrigger: {
-        trigger: '.card3',
-        start: 'top 15%',
-        end: 'bottom 15%',
-        scrub: true,
-    },
-})
+function animateCards(cards) {
+    cards.forEach(card => {
+        gsap.to(card, {
+            scale: 0.6,
+            opacity: 0,
+            scrollTrigger: {
+                trigger: card,
+                start: 'top 15%',
+                end: 'bottom 15%',
+                scrub: true,
+            },
+        });
+    });
+}
+
+const cards = ['.card1', '.card2', '.card3', '.card4', '.card5', '.card6', '.card7', '.card8'];
+animateCards(cards);
